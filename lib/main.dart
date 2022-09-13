67f261b7eb9a70cc11c2482c67faa55c68dc87af
Temi <mac@MACs-MacBook-Pro.local>
2022-09-13 15:11:54 -0700

    Created project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+
+
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlutterNetworking());
+
+class FlutterNetworking extends StatelessWidget {
+  const FlutterNetworking({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
